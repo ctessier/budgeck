0670f4ce7b153a3163fdd581435911c3efb21e99
ctessier <ctessier@users.noreply.github.com>
2015-12-05 22:57:04 +0100

    Update budgets and incomes progress bar styling

--- resources/views/accounts/month/budgets.blade.php
+++ resources/views/accounts/month/budgets.blade.php
@@ -1,17 +1,22 @@
 <div class="columns large-12">
-    <div data-tab-id="budgets" class="tab-content content active">
+    <div data-tab-id="budgets" class="tab-content content active clearfix">
         @foreach ($budgets as $budget)
         {{--*/ $amountSpent = $budget->getAmountSpent() /*--}}
         <div class="budget">
-            <span class="title">{{$budget->title}}</span>
-            <div class="progress-bar-container">
-                <div class="progress-bar" style="width:{{($amountSpent / $budget->amount) * 100}}%">
-                    @if ($amountSpent > 0)
-                    <span class="spent">{{$amountSpent}}</span>
-                    @endif
+            <div class="title">{{$budget->title}}</div>
+            <div class="progress">
+                <div class="progress-bar-container">
+                    <div class="progress-bar {{($amountSpent <= $budget->amount) ? 'green' : 'red'}}" style="width:{{min(100, ($amountSpent / $budget->amount) * 100)}}%">
+                        <div class="current-amount">
+                            @if ($amountSpent > 0)
+                            &euro; {{$amountSpent}}
+                            @endif
+                        </div>
+                    </div>
                 </div>
-                <span class="amount">{{$budget->amount}}</span>
-            </div>
+                <span class="amount">&euro; {{$budget->amount}}</span>
+            </div>                
+            <div class="actions"></div>
         </div>
         @endforeach
     </div>
--- resources/views/accounts/month/incomes.blade.php
+++ resources/views/accounts/month/incomes.blade.php
@@ -2,13 +2,15 @@
     <div data-tab-id="incomes" class="tab-content content">
         @foreach ($incomes as $income)
         <div class="income">
-            <span class="title">{{$income->title}}</span>
-            <div class="progress-bar-container">
-                <div class="progress-bar" style="width:100%">
-                    <span class="spent"></span>
+            <div class="title">{{$income->title}}</div>
+            <div class="progress">
+                <div class="progress-bar-container">
+                    <div class="progress-bar green" style="width:{{($income->credit_date !== null) ? '100' : '0'}}%">                        
+                    </div>
                 </div>
-                <span class="amount">{{$income->amount}}</span>
-            </div>
+                <span class="amount">&euro; {{$income->amount}}</span>
+            </div>                
+            <div class="actions"></div>
         </div>
         @endforeach
     </div>
