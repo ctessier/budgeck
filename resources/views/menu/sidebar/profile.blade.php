728dcbc9637e4b48d62884d3244d5fedab10ced5
ctessier <ctessier@users.noreply.github.com>
2016-02-01 23:59:33 +0100

    Update style sub menu

--- resources/views/menu/sidebar/profile.blade.php
+++ resources/views/menu/sidebar/profile.blade.php
@@ -3,12 +3,12 @@
         <li class="{!! (Route::currentRouteNamed('profile')) ? 'active' : '' !!}">
             {!! HTML::linkRoute('profile', 'Mon profil') !!}
         </li>
-        <li class="{!! (Route::currentRouteNamed('accounts') || Route::currentRouteName('accounts.getAccount')) ? 'active' : '' !!}">
+        <li class="{!! (Route::currentRouteNamed('accounts') || Route::currentRouteNamed('accounts.getAccount')) ? 'active' : '' !!}">
             {!! HTML::linkRoute('accounts', 'Mes comptes') !!}
             @if (Route::currentRouteNamed('accounts') || Route::currentRouteName('accounts.getAccount'))
             <ul>
                 @foreach ($user->accounts as $account)
-                <li>
+                <li class="{!! (Route::currentRouteNamed('accounts.getAccount') && Route::getCurrentRoute()->getParameter('account_id') == $account->id) ? 'active' : '' !!}">
                     {!! HTML::linkRoute('accounts.getAccount', $account->name, ['id' => $account->id]) !!}
                 </li>
                 @endforeach
