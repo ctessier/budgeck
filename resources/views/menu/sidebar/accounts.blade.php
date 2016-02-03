da7e59473bf7012e2d88673dc5146fb458e5b037
ctessier <ctessier@users.noreply.github.com>
2016-02-03 23:11:32 +0100

    Update sidebar accounts to condition links

--- resources/views/menu/sidebar/accounts.blade.php
+++ resources/views/menu/sidebar/accounts.blade.php
@@ -1,11 +1,11 @@
-<div class="columns large-4">    
+<div class="columns large-4">   
     <ul class="tabs">
         <li class="{!! ($account_id == 'all') ? 'active' : '' !!}">
-            {!! HTML::linkRoute('history', 'Tous mes comptes', 'all') !!}
+            {!! HTML::linkRoute(Route::currentRouteName(), 'Tous mes comptes', ['account_id' => 'all', 'year' => Route::getCurrentRoute()->getParameter('year'), 'month' => Route::getCurrentRoute()->getParameter('month')]) !!}
         </li>
         @foreach ($user->accounts as $account)
         <li class="{!! ($account_id == $account->id) ? 'active' : '' !!}">
-            {!! HTML::linkRoute('history', $account->name, $account->id) !!}
+            {!! HTML::linkRoute(Route::currentRouteName(), $account->name, ['account_id' => $account->id, 'year' => Route::getCurrentRoute()->getParameter('year'), 'month' => Route::getCurrentRoute()->getParameter('month')]) !!}
         </li>
         @endforeach
     </ul>
