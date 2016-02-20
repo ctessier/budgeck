74fa6b5d2ff868adf912d6d9cef6f2b76f51cb18
ctessier <ctessier@users.noreply.github.com>
2016-02-20 14:24:48 +0100

    Update style for account list balance

--- resources/views/accounts/list-accounts.blade.php
+++ resources/views/accounts/list-accounts.blade.php
@@ -4,7 +4,10 @@
         <a href="{{route('accounts.getAccount', ['id' => $account->id])}}">
             {{--*/ $accountBalance = $account->getCurrentBalance() /*--}}
             {{--*/ $accountLeft = $account->getLeftOvers() /*--}}
-            <span class="account-balance {{($accountBalance < 0) ? 'negative' : 'positive'}}">&euro; {{$accountBalance}}<br />&euro; {{$accountLeft}}</span>
+            <span class="account-balance">
+                <span class="{{($accountBalance < 0) ? 'negative' : 'positive'}}">&euro; {{$accountBalance}}</span>
+                <span class="{{($accountLeft < 0) ? 'negative' : 'positive'}}">&euro; {{$accountLeft}}</span>
+            </span>
             <span class="account-name">{{$account->name}}</span>
             <span class="account-description">{{$account->description}}</span>
         </a>
