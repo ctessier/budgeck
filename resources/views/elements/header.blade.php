f1288d6d349c451a0385fd3d335893cd3bc4ff61
ctessier <ctessier@users.noreply.github.com>
2016-02-20 13:10:52 +0100

    Update to first bêta version

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class="row">
         <div class="columns small-3">
-            <a href="{{url('/')}}" class="logo">{{ Config::get('budgeck.appName') }} <span class="version">{{ Config::get('budgeck.version') }}</span></a>
+            <a href="{{url('/')}}" class="logo">{{ Config::get('budgeck.appName') }} <span class="version">bêta</span></a>
         </div>
         <div class="columns small-9">
             @include('menu.default')
