bda2eafd482deef47e98f5b579fa3554550a48f5
ctessier <clement.tessier@ymail.com>
2016-06-04 13:05:21 +0200

    Fix #7

--- resources/views/elements/month-selector.blade.php
+++ resources/views/elements/month-selector.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group month-selector">
     @for ($i = $baseMonth-config('budgeck.aheadness') ; $i <= $baseMonth+config('budgeck.aheadness') ; ++$i)
-        {{--*/ $month = \Carbon\Carbon::createFromDate($baseYear, $i, null) /*--}}
-        <a class="{{ (isset($transaction) && $month->month === $transaction->month) || (!isset($transaction) && $month->month == $baseMonth) ? 'selected' : '' }}" data-month="{{ $month->month }}" data-year="{{ $month->year }}">{{ ucfirst($month->formatLocalized('%B')) }}</a>
+        {{--*/ $month = \Carbon\Carbon::createFromDate($baseYear, $i, 1) /*--}}
+        <a class="{{ (isset($transaction) && $month->month === $transaction->month) || (!isset($transaction) && $month->month === $baseMonth) ? 'selected' : '' }}" data-month="{{ $month->month }}" data-year="{{ $month->year }}">{{ ucfirst($month->formatLocalized('%B')) }}</a>
     @endfor
     {!! Form::hidden('month', isset($transaction) ? $transaction->month : $baseMonth, ['data-month' => 'true']) !!}
     {!! Form::hidden('year', isset($transaction) ? $transaction->year : $baseYear, ['data-year' => 'true']) !!}
