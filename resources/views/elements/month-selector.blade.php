5fd42fd78c23336da4854c9651d581d9656cf125
ctessier <clement.tessier@ymail.com>
2016-06-05 22:17:09 +0200

    Update month selector

--- resources/views/elements/month-selector.blade.php
+++ resources/views/elements/month-selector.blade.php
@@ -1,8 +1,11 @@
+{{--*/ $month = isset($transaction) ? $transaction->month : Carbon\Carbon::now()->month /*--}}
+{{--*/ $year = isset($transaction) ? $transaction->year : Carbon\Carbon::now()->year /*--}}
+{{--*/ $baseTime = isset($transaction) ? $transaction->created_at : Carbon\Carbon::now() /*--}}
 <div class="form-group month-selector">
-    @for ($i = $baseMonth-config('budgeck.aheadness') ; $i <= $baseMonth+config('budgeck.aheadness') ; ++$i)
-        {{--*/ $month = \Carbon\Carbon::createFromDate($baseYear, $i, 1) /*--}}
-        <a class="{{ (isset($transaction) && $month->month === $transaction->month) || (!isset($transaction) && $month->month === $baseMonth) ? 'selected' : '' }}" data-month="{{ $month->month }}" data-year="{{ $month->year }}">{{ ucfirst($month->formatLocalized('%B')) }}</a>
+    @for ($i = $baseTime->month - config('budgeck.aheadness') ; $i <= $baseTime->month + config('budgeck.aheadness') ; ++$i)
+        {{--*/ $currentTime = \Carbon\Carbon::createFromDate($baseTime->year, $i, 1) /*--}}
+        <a class="{{ ($currentTime->month === $month && $currentTime->year === $year) ? 'selected' : '' }}" data-month="{{ $currentTime->month }}" data-year="{{ $currentTime->year }}">{{ ucfirst($currentTime->formatLocalized('%B')) }}</a>
     @endfor
-    {!! Form::hidden('month', isset($transaction) ? $transaction->month : $baseMonth, ['data-month' => 'true']) !!}
-    {!! Form::hidden('year', isset($transaction) ? $transaction->year : $baseYear, ['data-year' => 'true']) !!}
+    {!! Form::hidden('month', $month, ['data-month' => 'true']) !!}
+    {!! Form::hidden('year', $year, ['data-year' => 'true']) !!}
 </div>
