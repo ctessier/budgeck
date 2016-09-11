3ed3d07dd84c72834456cb107e11030c9fd6ee16
ctessier <clement.tessier@ymail.com>
2016-09-11 22:07:49 +0200

    Update months budgets page styling

--- resources/views/accounts/account_budgets/index.blade.php
+++ resources/views/accounts/account_budgets/index.blade.php
@@ -1,5 +1,6 @@
-@if ($account->account_budgets->count() > 0)
-<table>
+{{--*/ $budgets = $account->account_budgets /*--}}
+@if ($budgets->count() > 0)
+<table class="ui table">
     <thead>
         <tr>
             <th>Titre</th>
@@ -9,20 +10,30 @@
         </tr>
     </thead>
     <tbody>
-        @foreach ($account->account_budgets as $account_budget)
+        {{--*/ $budgetsTotalAmount = 0 /*--}}
+        @foreach ($budgets as $account_budget)
+        {{--*/ $budgetsTotalAmount += $account_budget->amount /*--}}
         <tr>
             <td>{{ $account_budget->title }}</td>
             <td>{{ $account_budget->description }}</td>
             <td>@amount($account_budget->amount)</td>
             <td class="align-center">
-                {!! HTML::linkRoute('accounts.account_budgets.edit', 'Modifier', ['accounts' => $account->id, 'budgets' => $account_budget->id], ['class' => 'btn-tiny radius', 'data-use-lightbox' => 'true']) !!}
+                {!! HTML::linkRoute('accounts.account_budgets.edit', 'Modifier', ['accounts' => $account->id, 'budgets' => $account_budget->id], ['class' => 'ui mini button', 'data-use-lightbox' => 'true']) !!}
                 {!! Form::open(['method' => 'delete', 'route' => ['accounts.account_budgets.destroy', $account->id, $account_budget->id], 'style' => 'display:inline;', 'data-use-confirm' => 'true', 'data-confirm-message' => 'Souhaitez-vous définitivement supprimer ce budget ? Cela affectera seulement les budgets des mois futurs.']) !!}
-                    <a class="btn-tiny btn-red radius" data-form-submit="true">Supprimer</a>
+                    <a class="ui mini button" data-form-submit="true">Supprimer</a>
                 {!! Form::close() !!}
             </td>
         </tr>
         @endforeach
     </tbody>
+    <tfoot>
+        <tr>
+            <th>{{$budgets->count()}} budget(s)</th>
+            <th></th>
+            <th>@amount($budgetsTotalAmount)</th>
+            <th>{!! HTML::linkRoute('accounts.account_budgets.create', 'Ajouter un budget', ['accounts' => $account->id], ['class' => 'ui mini button', 'data-use-lightbox' => 'true']) !!}</th>
+        </tr>
+    </tfoot>
 </table>
 @else
 <p>Ajouter des budgets pour commencer à gérer les finances de ce compte.</p>
