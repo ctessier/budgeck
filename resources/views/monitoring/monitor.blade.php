60d1657628b366bacd0a8de5465b822f6b777f00
ctessier <clement.tessier@ymail.com>
2016-09-11 22:24:40 +0200

    Update months budgets page styling

--- resources/views/monitoring/monitor.blade.php
+++ resources/views/monitoring/monitor.blade.php
@@ -3,49 +3,45 @@
 @section('title', 'Suivi mensuel')
 
 @section('content')
-    <div class="columns large-4"></div>
-    <div class="columns large-8">
-        <h3>{{ $month_title }}</h3>
-        {!! HTML::linkRoute('accounts.budgets.create', 'Ajouter un budget', ['accounts' => $current_account->id, 'year' => $year, 'month' => $month], ['class' => 'btn-tiny radius', 'data-use-lightbox' => 'true']) !!}
-        @if ($budgets->count() === 0)
-            <div class="alert align-center">
-                Aucun budget à afficher
-            </div>
-        @else
-            <div class="">
-                @foreach ($budgets as $budget)
-                    <div class="budget">
-                        {{-- Budget infos --}}
-                        <span class="title">{{ $budget->title }}</span>
-                        <span class="description">{{ $budget->description }}</span>
-                        <span class="actions">
-                            {!! HTML::linkRoute('accounts.budgets.show', 'Voir les transactions', ['accounts' => $budget->account->id, 'budgets' => $budget->id], ['class' => 'btn-tiny radius', 'data-use-lightbox' => 'true']) !!}
-                            {!! HTML::linkRoute('accounts.budgets.edit', 'Modifier', ['accounts' => $budget->account->id, 'budgets' => $budget->id], ['class' => 'btn-tiny radius', 'data-use-lightbox' => 'true']) !!}
-                            {!! Form::open(['method' => 'delete', 'route' => ['accounts.budgets.destroy', $budget->account->id, $budget->id], 'style' => 'display:inline;', 'data-use-confirm' => 'true', 'data-confirm-message' => 'Souhaitez-vous définitivement supprimer ce budget ?']) !!}
-                            <a class="btn-tiny btn-red radius" data-form-submit="true">Supprimer</a>
-                            {!! Form::close() !!}
-                        </span>
-                        {{-- Budget progress bar --}}
-                        {{--*/ $totalAmountSpent = $budget->getAmountSpent() /*--}}
-                        {{--*/ $effectiveAmountSpent = $budget->getAmountSpent(Budgeck\Models\Transaction::EFFECTIVE) /*--}}
-                        <div class="progress-bar-container">
-                            <div class="progress-bar awaiting {{($totalAmountSpent > $budget->amount) ? 'full' : ''}}"
-                                 style="width:{{min(100, ($totalAmountSpent / $budget->amount) * 100)}}%">
-                                <div class="current-amount">
-                                    @amount($totalAmountSpent)
+<div class="ui grid">
+    @include('menu.sidebar.months')
+    <div class="twelve wide column">
+        <div class="ui segment">
+            <h3>{{ $month_title }}</h3>
+            @if ($budgets->count() === 0)
+                <div class="alert align-center">
+                    Aucun budget à afficher
+                </div>
+            @else
+                <div class="ui three doubling cards">
+                    @foreach ($budgets as $budget)
+                        {{--*/ $progressPourcent = min(100, ($budget->getAmountSpent() / $budget->amount) * 100) /*--}}
+                        <div class="card">
+                            <div class="content">
+                                <div class="header">{{$budget->title}}</div>
+                                <div class="meta">{{$budget->description}} - {{$budget->transactions->count()}} transaction(s)</div>
+                                <div class="description">
+                                    <div class="ui blue progress" data-percent="{{$progressPourcent}}">
+                                        <div class="bar"></div>
+                                        <div class="label">@amount($budget->getAmountSpent()) / @amount($budget->amount)</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="progress-bar {{($effectiveAmountSpent <= $budget->amount) ? 'green' : 'red'}}"
-                                 style="width:{{min(100, ($effectiveAmountSpent / $budget->amount) * 100)}}%">
-                                <div class="current-amount">
-                                    @amount($effectiveAmountSpent)
-                                </div>
+                            <div class="ui bottom attached button">
+                                <i class="add icon"></i>
+                                Transaction
                             </div>
-                            <span class="amount">@amount($budget->amount)</span>
                         </div>
-                    </div>
-                @endforeach
-            </div>
-        @endif
+                    @endforeach
+                </div>
+            @endif
+            <!--{!! HTML::linkRoute('accounts.budgets.create', 'Ajouter un budget', ['accounts' => $current_account->id, 'year' => $year, 'month' => $month], ['class' => 'ui button', 'data-use-lightbox' => 'true']) !!}-->
+        </div>
     </div>
+</div>
+<script>
+    $('.progress').progress({
+        autoSuccess: false
+    });
+</script>
 @stop
