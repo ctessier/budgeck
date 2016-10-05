d9d9a6e576bd5e57fdd788d971608d8894f35023
Clément Tessier <clement.tessier@ymail.com>
2016-10-05 12:59:27 +0100

    Add foundation-datepicker library

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ elixir.config.registerWatcher("default", "resources/**");
 
 elixir(function(mix) {
     mix
+        .copy('node_modules/foundation-datepicker/css/foundation-datepicker.min.css', 'public/assets/css/foundation-datepicker.min.css')
+        .copy('node_modules/foundation-datepicker/js/foundation-datepicker.min.js', 'public/assets/js/foundation-datepicker.min.js')
         .copy('bower_components/semantic/dist/semantic.js', 'public/assets/js/semantic.js')
         .copy('bower_components/semantic/dist/semantic.css', 'public/assets/css/semantic.css')
         .copy('bower_components/semantic/dist/themes/default', 'public/assets/css/themes/default')
@@ -31,6 +33,8 @@ elixir(function(mix) {
             'public/assets/js/semantic.js',
             'public/assets/js/jquery.js',
             'public/assets/js/script.js',
-            'public/assets/js/ajax-modal.js'
+            'public/assets/js/ajax-modal.js',
+            'public/assets/css/foundation-datepicker.min.css',
+            'public/assets/js/foundation-datepicker.min.js'
         ]);
 });
