0fa7d30d1ca480f35c79539e057eece11bb886af
ctessier <clement.tessier@ymail.com>
2016-10-12 20:59:03 +0200

    Fixed styling in app/Http/Kernel.php and app/Http/Middleware/Auth.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,8 +26,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
-        'auth' => \Budgeck\Http\Middleware\Authenticate::class,
+        'auth'       => \Budgeck\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'guest' => \Budgeck\Http\Middleware\RedirectIfAuthenticated::class,
+        'guest'      => \Budgeck\Http\Middleware\RedirectIfAuthenticated::class,
     ];
 }
