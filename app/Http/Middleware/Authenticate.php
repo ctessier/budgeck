c845b8f7c1ee8647cc7f473d8ffbe60bcb1b8217
ctessier <clement.tessier@ymail.com>
2016-10-12 21:01:11 +0200

    Fix styling in middleware

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -30,7 +30,7 @@ class Authenticate
      * Handle an incoming request.
      *
      * @param \Illuminate\Http\Request  $request
-     * @param \Closure  $next
+     * @param \Closure                  $next
      *
      * @return mixed
      */
