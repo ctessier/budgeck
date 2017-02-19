dd02a7f02a54134219f039e7669be5cf0066728a
ctessier <clement.tessier@ymail.com>
2017-02-19 15:49:52 +0100

    Add event to create new transaction on recurrent transaction create

--- app/Models/RecurrentTransaction.php
+++ app/Models/RecurrentTransaction.php
@@ -2,6 +2,8 @@
 
 namespace Budgeck\Models;
 
+use Carbon\Carbon;
+
 /**
  * Class RecurrentTransaction
  *
@@ -57,4 +59,45 @@ class RecurrentTransaction extends BaseModel
     {
         return $this->hasMany($this->getBaseNamespace().'\Transaction');
     }
+
+    /**
+     * Return the recurrent transaction date as for today.
+     *
+     * @return \Carbon\Carbon
+     */
+    public function getCurrentMonthDate()
+    {
+        return Carbon::now()->day($this->day);
+    }
+
+    /**
+     * Return the right budget according to the current month.
+     *
+     * @return \Budgeck\Models\Budget|null
+     */
+    public function getCurrentBudget()
+    {
+        // Return null if the recurrent transaction has no budget
+        if (!$this->account_budget) {
+            return null;
+        }
+
+        // Determine the transaction date according to if next_month is enable or not
+        $date = $this->getCurrentMonthDate();
+        if ($this->next_month) {
+            $date->addMonth(1);
+        }
+
+        // Fetch the budget according to the year and month
+        $budget = $this->account_budget->budgets()->where('year', $date->year)->where('month', $date->month)->getResults();
+        if ($budget = $budget->first()) {
+            return $budget;
+        } else {
+            return null;
+        }
+    }
 }
+
+RecurrentTransaction::created(function ($recurrentTransaction) {
+    Transaction::createFromRecurrentTransaction($recurrentTransaction);
+});
