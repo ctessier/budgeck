2ac925e969c6a9a432a255a118dffd926a69489a
ctessier <clement.tessier@ymail.com>
2017-02-22 22:06:21 +0100

    Make monitoring screen stackable

--- resources/views/monitoring/monitor.blade.php
+++ resources/views/monitoring/monitor.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Suivi mensuel')
 
 @section('content')
-<div class="ui grid">
+<div class="ui stackable grid">
     @include('menu.sidebar.months')
     <div class="twelve wide column">
         <div class="ui segment">
@@ -13,7 +13,7 @@
                     Aucun budget à afficher
                 </div>
             @else
-                <div class="ui three doubling cards">
+                <div class="ui three stackable cards">
                     @foreach ($budgets as $budget)
                         {{--*/ $progressPourcent = min(100, ($budget->getAmountSpent() / $budget->amount) * 100) /*--}}
                         <div class="card">
